d439525f189edcaa8d37820357f839fb9d53cdea
dakpangawang <ngwngdkp@gmail.com>
2023-01-14 23:12:58 -0500

    HW_SL06

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -2,33 +2,57 @@ package baseUtil;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.HomePage;
+import utils.Configuration;
+import static utils.IConstant.*;
 
 public class BaseClass {
 	public WebDriver driver;
 	public HomePage homePage;
+	protected JavascriptExecutor js;
+	protected Dimension dimension;
+	protected Actions actions;
+	protected Select select;
+	Configuration config;
+	protected WebDriverWait wait;
+
 	@BeforeMethod
-	public void setUp () {
+	public void setUp() {
+		config = new Configuration();
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		js = (JavascriptExecutor) driver;
+		actions = new Actions(driver);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("https://enthrallit.com/");
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		homePage = new HomePage(driver); 
-		
+		long pageLoadTime = Long.parseLong(config.getProperty(PAGELOAD_WAIT));
+		long implicitlyWait = Long.parseLong(config.getProperty(IMPLICITLY_WAIT));
+		long explicitlyWait = Long.parseLong(config.getProperty(EXPLICITLY_WAIT));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadTime));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitlyWait));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(explicitlyWait));
+		initClasses();
+
 	}
-	
+
+	private void initClasses() {
+		homePage = new HomePage(driver);
+	}
+
 	@AfterMethod
-	public void tearUp () {
+	public void tearUp() {
 		driver.quit();
 	}
 
--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -5,62 +5,62 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import baseUtil.BaseClass;
+import static common.CommonActions.*;
 
-public class HomePageTest extends BaseClass{
-	JavascriptExecutor js;
-	Dimension dimension;
-	Actions actions;
-	Select select;
-	
-	@Test (enabled = false)
-	public void clickSelenium () throws InterruptedException {
+public class HomePageTest extends BaseClass {
+
+	@Test(enabled = false)
+	public void clickSelenium() throws InterruptedException {
 		Thread.sleep(3000);
 		dimension = new Dimension(800, 500);
 		driver.manage().window().setSize(dimension);
-		System.out.println("Window set size is: "+driver.manage().window().getSize());
+		System.out.println("Window set size is: " + driver.manage().window().getSize());
 		WebElement selenium = driver.findElement(By.xpath("//a[text()='Selenium']"));
-		js = (JavascriptExecutor)driver;
+		js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", selenium);
 		Thread.sleep(3000);
 	}
-	
-	@Test (enabled = false)
-	public void use_of_assert_method () {
-		WebElement logo = driver.findElement(By.xpath("//img[@src='/static/img/logo/With-text.png' and @alt='Enthrall IT']"));
+
+	@Test(enabled = false)
+	public void use_of_assert_method() {
+		WebElement logo = driver
+				.findElement(By.xpath("//img[@src='/static/img/logo/With-text.png' and @alt='Enthrall IT']"));
 		logo.isDisplayed();
-		//Assert.assertTrue(true, "logo is not present in the Enthrall it website.");
+		// Assert.assertTrue(true, "logo is not present in the Enthrall it website.");
 		Assert.assertFalse(false, "Logo is displayed in the Enthrall It website.");
-		
+
 	}
-	
-	@Test (enabled = false)
-	public void use_of_assertEquals_method () {
+
+	@Test(enabled = false)
+	public void use_of_assertEquals_method() {
 		String expected = "Enthrall IT";
 		String actual = driver.getTitle();
 		Assert.assertEquals(actual, expected, "HomePage tile doesnt match");
 	}
-	
-	@Test (enabled = false)
-	public void use_of_softAssertion_method () {
+
+	@Test(enabled = false)
+	public void use_of_softAssertion_method() {
 		String expected = "Enthrall IT";
 		String actual = driver.getTitle();
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertEquals(actual, expected, "HomePage tile doesnt match");
 		String currentUrl = driver.getCurrentUrl();
-		System.out.println("Current URL is: "+currentUrl);
+		System.out.println("Current URL is: " + currentUrl);
 	}
-	
+
 	// Hover Action 0r Mouse Hover Over Action
-	@Test (enabled = false)
-	public void use_of_actions () throws InterruptedException {
+	@Test(enabled = false)
+	public void use_of_actions() throws InterruptedException {
 		Thread.sleep(3000);
 		driver.get("https://www.amazon.com/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
@@ -69,15 +69,15 @@ public class HomePageTest extends BaseClass{
 		WebElement prime = driver.findElement(By.xpath("//span[text()='Prime']"));
 		actions.moveToElement(prime).build().perform();
 		Thread.sleep(3000);
-		
+
 	}
-	
+
 	// for drop down or all categories
 	// here using selectByIndex() method
-	@Test (enabled = true)
-	public void use_of_selectByIndex_forDropdown () throws InterruptedException {
+	@Test(enabled = false)
+	public void use_of_selectByIndex_forDropdown() throws InterruptedException {
 		WebElement selenium = driver.findElement(By.xpath("//a[text()='Selenium']"));
-		js = (JavascriptExecutor)driver;
+		js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click()", selenium);
 		Thread.sleep(3000);
 		WebElement dropDown = driver.findElement(By.xpath("(//select[@class='form-control'])[1]"));
@@ -86,36 +86,170 @@ public class HomePageTest extends BaseClass{
 		select = new Select(dropDown);
 		select.selectByVisibleText("Unemployed");
 		Thread.sleep(3000);
-		
-		
 	}
 
+	@Test(enabled = false)
+	public void use_of_logger() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.amazon.com/");
+		WebElement aboutUs = driver.findElement(By.xpath("(//a[text()='Best Sellers'])[1]"));
+		clickElement(aboutUs);
+		Thread.sleep(3000);
+	}
+
+	@Test(enabled = false)
+	public void sendKeys_by_javaScript() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.amazon.com/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		WebElement searchField = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		WebElement clickButton = driver.findElement(By.id("nav-search-submit-button"));
+		js.executeScript("arguments[0].value='Laptop'", searchField);
+		js.executeScript("arguments[0].click()", clickButton);
+		Thread.sleep(3000);
+
+	}
+
+	@Test(enabled = false)
+	public void hiddenElement_by_selenium() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://courses.letskodeit.com/practice");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.id("hide-textbox")).click();
+		driver.findElement(By.xpath("//input[@id='displayed-text']")).sendKeys("hello");
+	}
+
+	@Test(enabled = false)
+	public void hiddenElement_by_javaScript() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://courses.letskodeit.com/practice");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.id("hide-textbox")).click();
+		Thread.sleep(3000);
+		WebElement field = driver.findElement(By.xpath("//input[@id='displayed-text']"));
+		js.executeScript("arguments[0].value='hello'", field);
+		Thread.sleep(3000);
+		String s = (String) js.executeScript("return document.getElementById('displayed-text').value");
+		System.out.println("Value of Hidden Field: " + s + "\n");
+
+		// getting title of page using javaScript exucutor
+		String title = (String) js.executeScript("return document.title");
+		System.out.println("Tile of the page is: " + title);
+	}
+
+	// explicitly wait
+	// 1. using visibilityOfElementLocated() method condition
+	@Test(enabled = false)
+	public void explicitlyWait01() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.mountsinai.org/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//a[normalize-space(text())='Request an Appointment' and @class='hidden-xs dropdown']")))
+				.click();
+
+	}
+
+	// 2. using elementToBeClickable
+	@Test(enabled = false)
+	public void explicitlyWait02() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.mountsinai.org/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//a[normalize-space(text())='Request an Appointment' and @class='hidden-xs dropdown']")))
+				.click();
+
+	}
+
+	// 3. using visibilityOf()
+	@Test(enabled = false)
+	public void explicitlyWait03() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.mountsinai.org/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		WebElement requestAppointment = driver.findElement(
+				By.xpath("//a[normalize-space(text())='Request an Appointment' and @class='hidden-xs dropdown']"));
+		wait.until(ExpectedConditions.visibilityOf(requestAppointment)).click();
+
+	}
+
+	// 4. using presenceOfElementLocated()
+	@Test(enabled = false)
+	public void explicitlyWait04() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.mountsinai.org/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.presenceOfElementLocated(
+				By.xpath("//a[normalize-space(text())='Request an Appointment' and @class='hidden-xs dropdown']")))
+				.click();
+
+	}
+
+	// 5. using textToBePresentInElement()
+	@Test(enabled = false)
+	public void explicitlyWait05() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.get("https://www.mountsinai.org/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		WebElement requestAppointment = driver.findElement(By.xpath("//a[normalize-space(text())='Request an Appointment' and @class='hidden-xs dropdown']"));
+		wait.until(ExpectedConditions.textToBePresentInElement(requestAppointment, "Request an Appointment"));
+
+	}
+	
+	@Test (enabled = false)
+	public void scrollDown_actionsClass () throws InterruptedException {
+		actions.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
+		Thread.sleep(3000);
+		actions.keyDown(Keys.CONTROL).sendKeys(Keys.HOME).perform();
+		Thread.sleep(3000);
+	}
+	
+	@Test (enabled = true)
+	public void scrollDown_by_javaScript () throws InterruptedException {
+		Thread.sleep(3000);
+		js.executeScript("window.scrollBy(0,3000)", " ");
+		Thread.sleep(3000);
+		js.executeScript("window.scrollBy(0,-3000)", " ");
+		Thread.sleep(3000);
+		js.executeScript("history.go(0)", " "); // to refresh page
+		Thread.sleep(3000);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
